a7c95853453cd716a7854cca677bac012f223e4c
indegser <indegser@gmail.com>
2018-06-28 13:54:54 +0900

    style(global): add Apple Gothic and aliasing for fonts

--- js/common/css/global.styled.js
+++ js/common/css/global.styled.js
@@ -3,7 +3,12 @@ import { injectGlobal } from 'emotion';
 injectGlobal`
   html {
     font-size: 16px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'Noto Sans KR', 'Malgun Gothic', 'Dotum', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Dotum', sans-serif;
+  }
+
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   body {
