c95ecd905e127d66f73b979ff3347911d68cee3f
indegser <indegser@gmail.com>
2018-06-29 16:29:06 +0900

    style(footer): add spacing in footer and change bg color

--- js/common/components/organisms/Footer.jsx
+++ js/common/components/organisms/Footer.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import { css } from 'emotion';
 import { Container } from '../../../main/main.styled';
 import { Flex } from '../atom/Flex';
+import Box from '../atom/Box';
+import { Text } from '../atom/Text';
 
 class Footer extends React.Component {
   render() {
     return (
-      <footer css={`background-color: #3A3939; color: aaa;`}>
-        <Container py={16}>
-          <Flex>
-            Copyright 2018 EOSDAQ.com (Inc) All rights reserved.
-          </Flex>
-        </Container>
+      <footer>
+        <Box
+          bg="primaryDark"
+          color="whiteA7"
+          py={8}
+        >
+          <Container py={16}>
+            <Box
+              pb={24}
+            >
+              
+            </Box>
+            <Flex justifyContent="flex-end" >
+              <Text color="whiteA7" fontSize={12}>
+                Copyright 2018 EOSDAQ.com (Inc) All rights reserved.
+              </Text>
+            </Flex>
+          </Container>
+        </Box>
       </footer>
     )
   }
