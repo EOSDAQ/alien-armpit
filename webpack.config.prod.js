51f48b0e134f512898907e6793f1ca5ebdc7b090
indegser <indegser@eosdaq.com>
2018-07-12 12:39:30 +0900

    chore(webpack): add copy and clean webpack plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,22 @@
 const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const devConfig = require('./webpack.config.dev');
 
 const prodConfig = {
   mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin([
+      'dist',
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: 'public/',
+        to: '.',
+      },
+    ]),
+  ],
 };
 
 module.exports = merge(devConfig, prodConfig);
