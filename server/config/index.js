0bf0bd752e629848348f9c9152241ebd4ec5217a
pebf <newpeople27@gmail.com>
2018-08-01 10:23:33 +0900

    feat(chore): add packaging(by pkg) feature

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,17 @@
+/* pkg 실행 시 사용될 모듈을 명시하지 않으면 package에 포함이 되지 않으므로
+  사용하지 않는 모듈도 로드 */
+const local = require('./local');
+const development = require('./development');
+const production = require('./production');
+
 const env = process.env.NODE_ENV || 'local';
-const configPath = `./${env}`;
-const config = require(configPath);
+let config = local;
+
+if (env === 'production') {
+  config = production;
+} else if (env === 'development') {
+  config = development;
+}
+
 config.env = env;
 module.exports = config;
