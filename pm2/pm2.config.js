8519a0f993914982ce0dda2a171a7eb3031226c9
yimchedong <yimchedong@eosdaq.com>
2018-09-03 18:51:48 +0900

    fix(pm2): remove useless pm2 config

--- pm2/pm2.config.js
+++ pm2/pm2.config.js
@@ -4,13 +4,8 @@ module.exports = {
     script: 'bin/run-web',
     wait_ready: true,
     watch: ['./server', './bin'],
-    env: {
-      NODE_ENV: 'local',
-      // watch: ['./server', './bin'],
-    },
     env_production: {
-      NODE_ENV: 'production',
       watch: false,
     },
-  }],
+  }], 
 };
