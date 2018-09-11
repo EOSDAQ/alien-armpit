effee97ae77ebadf32fce788b91b62dfe9c8430c
pebf <newpeople27@gmail.com>
2018-09-11 11:12:09 +0900

    feat(log) add request debug log

--- server/modules/request.js
+++ server/modules/request.js
@@ -1,22 +1,23 @@
 const axios = require('axios');
+const logger = require('./logger');
 
 const request = async (meth, url, data, opt) => {
   const method = meth ? meth.toLowerCase() : 'get';
-  const args = [ url ];
+  const args = [url];
   let options = opt || {};
   options.headers = options.headers || {};
   options = setJwtHeader(options);
-
-  if ([ 'post', 'put', 'patch' ].indexOf(method) > -1) {
+  if (['post', 'put', 'patch'].indexOf(method) > -1) {
     args.push(data);
   } else if (method === 'delete') {
     options.data = data;
   } else {
-    options.params = {data};
+    options.params = { data };
   }
 
+  logger.debug(`${method.toUpperCase()} ${url} data=${data && JSON.stringify(data)} options=${options && JSON.stringify(options)}`);
   args.push(options);
-  const response = await axios[method]( ...args );
+  const response = await axios[method](...args);
   const { status, data: _data } = response;
 
   return {
@@ -27,7 +28,7 @@ const request = async (meth, url, data, opt) => {
       resultCode: status,
       resultMsg: _data.resultMsg,
       resultData: _data.resultData,
-    }
+    },
   };
 };
 
